e36879b9f172cb33d865bf9a7f5d3e12530614ac
Veronika Koksharova <91394025+delioncourts@users.noreply.github.com>
2023-11-11 21:53:33 +0300

    feat: test hello for vercel

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -16,6 +16,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h2>Test Hello</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}

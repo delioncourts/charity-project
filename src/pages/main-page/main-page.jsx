4ff290596adee5f79023945d587190386d0fb08f
Veronika Koksharova <91394025+delioncourts@users.noreply.github.com>
2023-11-12 15:16:32 +0300

    feat: add logo, styles and main

--- src/pages/main-page/main-page.jsx
+++ src/pages/main-page/main-page.jsx
@@ -1,11 +1,13 @@
 import { Link } from 'react-router-dom';
 
 import styles from './main-page.module.css';
+import logo from '../../assets/logo.png';
 
 const MainPage = () => {
     return (
         <main className={styles.main}>
             <h1 className={styles.title}>Привет!</h1>
+            <img src={logo} alt="логотип команды 48" />
             <p className={styles.subtitle}>Предлагаем сыграть в игру Memory Game</p>
 
             <Link to='/'>Главная</Link>
--- src/pages/not-found-404/not-found-404.jsx
+++ src/pages/not-found-404/not-found-404.jsx
@@ -4,10 +4,12 @@ import { Link } from 'react-router-dom';
 const NotFound = () => {
     return (
         <main className={styles.main}>
+            <section className={styles.container}>
             <h1 className={styles.title}>ОШЫпКА</h1>
             <p className={styles.subtitle}>Команда 48 сожалеет, что всё так вышло и прикладывает все усилия, чтобы всё поправить.</p>
-            <p className={styles.subtitle}>Но это не точно</p>
+            <p className={styles.subtitle}>Но это не точно&rang;</p>
             <Link to='/' className={styles.link}>Меню</Link>
+            </section>
         </main>
     )
 }
